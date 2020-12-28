2374d1d5d2df506a14c16436980e51c4d5a2d7fc
Juan Carlos Serrano <juancarlos.serrano.chicharro@bbvaitspain.com>
2020-12-28 16:10:53 +0100

    Add java language

---
+++ java/src/main/java/es/juanc/katas/fizzbuzz/App.java
@@ -0,0 +1,20 @@
+package es.juanc.katas.fizzbuzz;
+
+import java.util.logging.Logger;
+
+// @Log
+public class App {
+
+    private static Logger log;
+    static {
+        System.setProperty("java.util.logging.config.file",
+                App.class.getClassLoader().getResource("logging.properties").getFile()
+        );
+        log = Logger.getLogger(App.class.getName());
+    }
+
+    public static void main(String[] args) {
+
+        log.info("Hello world!!");
+    }
+}
